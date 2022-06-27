f0659c88086d191ae7e7a6e03c67680c5306f219
Shayan Ameen <shayan.ameen.65@gmail.com>
2022-06-27 14:47:06 +0500

    implemented navigation component

---
+++ src/routes/Navigation.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../assets/crown.svg';
+import './Navigation.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='navigation-links-container'>
+          <Link className='navigation-link' to='/shop'>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
